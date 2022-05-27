4468b8625c777cc658f5607466081a84690847ed
anabibelforscher <anaolivercontact@gmail.com>
2022-05-27 14:45:53 -0300

    Singleton

---
+++ src/dio/designpatterns/gof/SingletonLazy.java
@@ -0,0 +1,16 @@
+package dio.designpatterns.gof;
+
+public class SingletonLazy {
+
+    private static SingletonLazy instance;
+
+    private SingletonLazy(){
+        super();
+    }
+
+    public static SingletonLazy getInstance(){
+        if (instance == null)
+            instance = new SingletonLazy();
+        return instance;
+    }
+}
